10c9695318cc76e35a40287e6329e0e90860a435
Roger Neel <pair+roger@pivotallabs.com>
2010-11-07 18:21:34 -0800

    adding in rspec generators

---
+++ generators/jasmine-fixtures/templates/spec/controllers/jasmine_fixture_creators.rb
@@ -0,0 +1,29 @@
+require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+
+#####
+# Modify this file to create new Jasmine Fixtures!
+# We have provided an example for you to crib off.
+
+# Basically:
+# 1) Visit a route with your controller action
+# 2) Make sure it's successful
+# 3) save_fixture with the response you want and the name of the fixture
+#####
+
+##### Using save_fixture
+# html_for will only take the <body> tag and replace it with a <div>
+# to avoid nested <body> tags within the jasmine runner
+
+# If you have custom responses (ajax partials, JSON, etc),
+# you can subvert this and use response.body or whatever.
+#####
+
+
+##### Example
+describe UsersController do
+  it "generates a new user page" do
+    get :new
+    response.should be_success
+    save_fixture(html_for('body'), 'user-signup-page')
+  end
+end
